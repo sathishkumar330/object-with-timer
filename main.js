f747ee70993ea244f31264cb29570b53a492d4e7
sathishkumar330 <svksathish41@gmail.com>
2023-03-24 16:00:22 +0530

    add

---
+++ main.js
@@ -0,0 +1,27 @@
+var library=[
+			{
+				author:"bil gates",
+				title:"the road ahead",
+				readingStatus:true
+			},
+			{
+				author:"steve jobs",
+				title:"walter isaacson",
+				readingStatus:true
+			},	
+			{
+				author:"suzanne colins",
+				title:"the final book of the hunger",
+				readingStatus:false
+			}];	
+			
+			library.forEach(function(e){
+				console.log(e);
+	  			});
+			if(s.readindStatus===true){
+					console.log("this book is already readed");
+					}
+			else{
+					console.log("this book is not readed");
+					}
+				
